835b3e5e41ce68c4ff76704c40585a78148202ff
Benny C. Wong <bwong337@gmail.com>
2014-07-31 02:00:11 -0700

    even more tests

---
+++ spec/features/overview_feature_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+feature "Overview Features" do
+	before :each do
+    DatabaseCleaner.clean
+    visit root_url
+    click_link "Sign in with Twitter"
+  end
+  after :each do
+    DatabaseCleaner.clean
+  end
+
+  scenario "User clicks on the first tweet username" do
+    find(:xpath, '//a[@id="nickname-0"]').click
+    expect(page).to have_selector("#user-page")
+  end
+
+  # scenario "User clicks on the tweet full name" do
+  #   find(:xpath, '//a[@id="fullname-0"]').click
+  # end
+
+  scenario "User clicks on the first tweet body hashtag" do
+    first(:xpath, '//a[@class="tweet-hashtag"]').click
+
+    expect(page).to have_selector("#overview-page")    
+
+  end
+
+  scenario "User clicks on the first tweet body nickname" do
+    first(:xpath, '//a[@class="tweet-screen-name"]').click
+    expect(page).to have_selector("#user-page")
+  end
+
+
+end
+
+
+
---
+++ spec/features/user_feature_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature "Users Page Features" do
+	before :each do
+    DatabaseCleaner.clean
+    visit root_url
+    click_link "Sign in with Twitter"
+    visit user_path("bwong337")
+  end
+  after :each do
+    DatabaseCleaner.clean
+  end
+
+  scenario "User clicks on the first tweet username" do
+    find(:xpath, '//a[@id="nickname-0"]').click
+    expect(page).to have_selector("#user-page")
+  end
+
+  scenario "User clicks on the first tweet body hashtag" do
+    first(:xpath, '//a[@class="tweet-hashtag"]').click
+    expect(page).to have_selector("#overview-page")    
+  end
+
+  scenario "User clicks on the first tweet body nickname" do
+    first(:xpath, '//a[@class="tweet-screen-name"]').click
+    expect(page).to have_selector("#user-page")
+  end
+
+
+end
+
+
+
